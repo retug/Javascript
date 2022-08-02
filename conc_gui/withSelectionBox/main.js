0c4bda62e6be564843026f8bd89f8df28e557e50
Austin Guter <45467091+retug@users.noreply.github.com>
2022-08-01 22:08:30 -0600

    Add files via upload
    update selection box

--- conc_gui/withSelectionBox/main.js
+++ conc_gui/withSelectionBox/main.js
@@ -3,51 +3,33 @@ import {OrbitControls} from 'https://unpkg.com/three@0.126.1/examples/jsm/contro
 import { SelectionBox } from 'https://unpkg.com/three@0.126.1/examples/jsm/interactive/SelectionBox.js';
 import { SelectionHelper } from 'https://unpkg.com/three@0.126.1/examples/jsm/interactive/SelectionHelper.js';
 
-console.log(SelectionBox)
-console.log(SelectionHelper)
+const concGui = document.querySelector('#concGui');
+console.log(concGui)
+
 const scene = new THREE.Scene()
-const camera = new THREE.PerspectiveCamera(75, innerWidth/innerHeight, 0.1, 1000)
-const renderer = new THREE.WebGLRenderer()
+const camera = new THREE.PerspectiveCamera(75, concGui.offsetWidth/concGui.offsetHeight, 0.1, 1000)
+const renderer = new THREE.WebGLRenderer({
+  antialias: true,
+  canvas: document.querySelector('canvas')
+})
 
 scene.background = new THREE.Color( 0xffffff );
 //sets the size of the the 3d window might need to be modified.
 // the minus 250 is to remove our node side heading
-renderer.setSize(innerWidth-250, innerHeight)
-document.getElementById("GUI").appendChild(renderer.domElement)
 
-///////////////////////////////////////////////////
-///////////////// create spehere //////////////////
-///////////////////////////////////////////////////
-const geometry = new THREE.SphereGeometry( 15, 32, 16 );
-const planeMaterial = new THREE.MeshPhongMaterial({
-  side: THREE.DoubleSide,
-  flatShading: THREE.FlatShading,
-  vertexColors: true,
-  color: 0xff8c69,
-})
-const sphere = new THREE.Mesh( geometry, planeMaterial );
-//scene.add( sphere );
 
-///////////////////////////////////////////////////
-//////////////// end sphere ///////////////////////
-///////////////////////////////////////////////////
 
-///////////////////////////////////////////////////
+renderer.setSize(concGui.offsetWidth, concGui.offsetHeight)
+renderer.setPixelRatio(window.devicePixelRatio)
+//document.getElementById("GUI").appendChild(renderer.domElement)
+
 //////////this the region of the dot///////////////
-///////////////////////////////////////////////////
-
 var dotGeometry = new THREE.BufferGeometry();
 dotGeometry.setAttribute( 'position', new THREE.Float32BufferAttribute( [0,0,0], 3 ) );
 var dotMaterial = new THREE.PointsMaterial( { size: 0.5, color: 0x00FF00 } );
 var dot = new THREE.Points( dotGeometry, dotMaterial );
 scene.add( dot );
-
-
-///////////////////////////////////////////////////
 /////////////////////////////end dot///////////////
-///////////////////////////////////////////////////
-
-console.log(dot)
 
 const light = new THREE.DirectionalLight(0xffffff, 1)
 light.position.set(0, -1, 5)
@@ -57,7 +39,10 @@ const backLight = new THREE.DirectionalLight(0xffffff, 1)
 backLight.position.set(0, 0, -5)
 scene.add(backLight)
 
-new OrbitControls(camera, renderer.domElement)
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.enableRotate = false;
+
+console.log(OrbitControls)
 camera.position.z = 50
 
 const axesHelper = new THREE.AxesHelper( 5 );
@@ -67,13 +52,13 @@ const size = 20;
 const divisions = 20;
 
 const gridHelper = new THREE.GridHelper( size, divisions );
-gridHelper.rotation.x=Math.PI/2;
+gridHelper.rotation.x=Math.PI/2; //gets grid oriented in XY axis
 scene.add( gridHelper );
 
 renderer.render( scene, camera );
 
 const selectionBox = new SelectionBox( camera, scene );
-const helper = new SelectionHelper( renderer, 'selectBox' );
+const helper = new SelectionHelper(selectionBox, renderer, 'selectBox' );
 
 
 document.addEventListener( 'pointerdown', function ( event ) {
@@ -83,12 +68,14 @@ document.addEventListener( 'pointerdown', function ( event ) {
     item.material.emissive.set( 0x000000 );
 
   }
-
+// #1 1/6 for coordinates on left hand side of screen
   selectionBox.startPoint.set(
-    ( event.clientX / window.innerWidth ) * 2 - 1,
-    - ( event.clientY / window.innerHeight ) * 2 + 1,
+    ((event.clientX - (window.innerWidth*1/6)) / concGui.offsetWidth)*2-1,
+    - ( event.clientY / concGui.offsetHeight )*2+1,
     0.5 );
-
+    //
+    console.log(((event.clientX - (window.innerWidth*1/6)) / concGui.offsetWidth)*2-1)
+    console.log(-( event.clientY / concGui.offsetHeight )*2+1)
 } );
 
 document.addEventListener( 'pointermove', function ( event ) {
@@ -102,9 +89,13 @@ document.addEventListener( 'pointermove', function ( event ) {
     }
 
     selectionBox.endPoint.set(
-      ( event.clientX / window.innerWidth ) * 2 - 1,
-      - ( event.clientY / window.innerHeight ) * 2 + 1,
+      //#2
+      ((event.clientX - (window.innerWidth*1/6)) / concGui.offsetWidth)*2-1,
+      - ( event.clientY / concGui.offsetHeight )*2+1,
       0.5 );
+      //
+      //console.log(((event.clientX - (window.innerWidth*1/6)) / concGui.offsetWidth) * 2 - 1)
+      //console.log(-( event.clientY / concGui.offsetHeight ))
 
     const allSelected = selectionBox.select();
 
@@ -120,16 +111,20 @@ document.addEventListener( 'pointermove', function ( event ) {
 
 document.addEventListener( 'pointerup', function ( event ) {
 
+  //#3
   selectionBox.endPoint.set(
-    ( event.clientX / window.innerWidth ) * 2 - 1,
-    - ( event.clientY / window.innerHeight ) * 2 + 1,
+    ((event.clientX - (window.innerWidth*1/6)) / concGui.offsetWidth)*2-1,
+    - ( event.clientY / concGui.offsetHeight)*2+1,
     0.5 );
 
   const allSelected = selectionBox.select();
+  //
+  //console.log(((event.clientX - (window.innerWidth*1/6)) / concGui.offsetWidth) * 2 - 1)
+  //console.log(-( event.clientY / concGui.offsetHeight ))
+  console.log(allSelected)
 
   for ( let i = 0; i < allSelected.length; i ++ ) {
-
-    allSelected[ i ].material.emissive.set( 0xffffff );
+    allSelected[ i ].material.color.set( 0xFF7F00);
 
   }
 
@@ -150,6 +145,7 @@ function addPoint() {
 function animate() {
   var X = document.getElementById( "X_Vals" ).value;
   var Y = document.getElementById( "Y_Vals" ).value;
+  //rendering the moving point on the screen, might need an id to filter
   dotGeometry.setAttribute( 'position', new THREE.Float32BufferAttribute( [X,Y,0], 3 ) );
   document.getElementById("addPointBtn").onclick = function(){
     addPoint();
