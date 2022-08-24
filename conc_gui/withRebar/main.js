e550a0e857614f5a27c50a02847d5515c15269ac
Austin Guter <45467091+retug@users.noreply.github.com>
2022-08-23 22:40:24 -0600

    Add files via upload

--- conc_gui/withRebar/main.js
+++ conc_gui/withRebar/main.js
@@ -82,6 +82,9 @@ document.getElementById('concGui').addEventListener( 'pointerdown', function ( e
     for (const pnt of allSelectedRebar) {
       pnt.material.color.setHSL( 0.0, 0.0, 0.5 );
     }
+    for (const pnt of allSelectedConc) {
+      pnt.material.color.set(0xE5E5E5);
+    }
     allSelectedPnts = []
     allSelectedRebar = []
     allSelectedConc = []
@@ -117,6 +120,9 @@ document.getElementById('concGui').addEventListener( 'pointermove', function ( e
         //selected point is 0xFF7F00
           allSelected[ i ].material.color.set( 0xFF7F00);        
         }
+        else if (allSelected[ i ].constructor.name == "Mesh") {
+          allSelected[ i ].material.color.set( 0xFF7F00);
+        }
     }
   }
   else {
@@ -139,6 +145,10 @@ document.getElementById('concGui').addEventListener( 'pointermove', function ( e
           //selected point is 0xFF7F00
           allSelected[ i ].material.color.set( 0xFF7F00);        
         }
+        else if (allSelected[ i ].constructor.name == "Mesh") {
+          allSelected[ i ].material.color.set( 0xFF7F00);
+        }
+
       }
     }
   }
@@ -178,12 +188,13 @@ if (event.ctrlKey) {
       Yinput.type = "Number";
       Xinput.value = Xpnt
       Yinput.value = Ypnt
-      Xdata = Xinput
-      Ydata = Yinput
-      Xdata.style ="width: 50%"
-      Ydata.style ="width: 50%"
+      Xinput.classList.add("numDropDown")
+      Yinput.classList.add("numDropDown")
+      Xdata.appendChild(Xinput) 
+      Ydata.appendChild(Yinput)
       row.appendChild(Xdata)
       row.appendChild(Ydata)
+      console.log(row)
       table.appendChild(row)  
     }
     // filtering for rebar selected
@@ -192,7 +203,6 @@ if (event.ctrlKey) {
       allSelectedRebar.push(allSelected[i])
       allSelected[ i ].material.color.set( 0xFF7F00);
       let table = document.getElementById("rebarData")
-      console.log(table)
       let row = document.createElement('tr');
       let Xpnt = allSelected[ i ].geometry.attributes.position.array[0] 
       let Ypnt = allSelected[ i ].geometry.attributes.position.array[1]
@@ -202,25 +212,60 @@ if (event.ctrlKey) {
       let barData = document.createElement('td')
       var Xinput = document.createElement("input");
       var Yinput = document.createElement("input");
-      var barDiainput = document.createElement("input");
+      //var barDiaInput = document.createElement("input");
+      var barDiaInput = document.createElement("select");
+
+      var array = [3,4,5,6,7,8,9,10,11,14,18]
+
+      for (var j = 0; j < array.length; j++) {
+        var option = document.createElement("option");
+        option.setAttribute("value", array[j]);
+        option.text = array[j];
+        barDiaInput.appendChild(option);
+      }
+
       Xinput.type = "Number";
       Yinput.type = "Number";
-      barDiainput.type = "Number";
+      //barDiaInput.type = "Number";
       Xinput.value = Xpnt
       Yinput.value = Ypnt
-      barDiainput.value = barDiaSelected
-      Xdata = Xinput
-      Ydata = Yinput
-      barData = barDiainput
-      Xdata.style ="width: 50%"
-      Ydata.style ="width: 50%"
-      barData.style = "width: 50%"
+      barDiaInput.value = barDiaSelected
+      Xinput.classList.add("numDropDown") 
+      Yinput.classList.add("numDropDown")
+      barDiaInput.classList.add("numDropDown")
+      Xdata.appendChild(Xinput) 
+      Ydata.appendChild(Yinput)
+      barData.appendChild(barDiaInput)
       row.appendChild(Xdata)
       row.appendChild(Ydata)
       row.appendChild(barData)
       table.appendChild(row)  
     }
+    else if (allSelected[ i ].constructor.name == "Mesh") {
+      allSelectedConc.push(allSelected[i])
+      allSelected[ i ].material.color.set( 0xFF7F00);
+      //adding to table
+      let table = document.getElementById("concData")
+      let row = document.createElement('tr');
+      let concPnt = allSelectedConc.length
+      console.log(concPnt)
+      
+      let concData = document.createElement('td')
+      concData.innerHTML = concPnt
+      row.appendChild(concData)
+      console.log(concData)
+      console.log(row)
+      table.appendChild(row)  
+    }
+
   }
+  var pnts = document.getElementById("pointsSelected")
+  console.log(allSelectedPnts)
+  pnts.innerHTML = (allSelectedPnts.length) 
+  var rebars = document.getElementById("rebarSelected")
+  rebars.innerHTML = allSelectedRebar.length
+  var concs = document.getElementById("concSelected")
+  concs.innerHTML = allSelectedConc.length
 }
   else {
     //resets the points in the scene
@@ -228,6 +273,8 @@ if (event.ctrlKey) {
     temptable.innerHTML = ''
     let rebartable = document.getElementById("rebarData")
     rebartable.innerHTML = ''
+    let conctable = document.getElementById("concData")
+    conctable.innerHTML = ''
     //end reset
     selectionBox.endPoint.set(
       ((event.clientX - (window.innerWidth*1/6)) / concGui.offsetWidth)*2-1,
@@ -242,27 +289,35 @@ if (event.ctrlKey) {
         allSelectedPnts.push(allSelected[i])
         //selected point is 0xFF7F00
         allSelected[ i ].material.color.set( 0xFF7F00);
-        console.log(allSelectedPnts)
+        
       }
       //this is rebar
       else if (allSelected[ i ].constructor.name == "Points" && allSelected[i].isReference != true && allSelected[i].isRebar == true) {
         allSelectedRebar.push(allSelected[i])
         //selected point is 0xFF7F00
         allSelected[ i ].material.color.set( 0xFF7F00);
-        console.log(allSelectedRebar)
+        
       }
+      else if (allSelected[ i ].constructor.name == "Mesh") {
+        allSelectedConc.push(allSelected[i])
+        allSelected[ i ].material.color.set( 0xFF7F00);
+        
+      }
+      console.log('you are in the conc area')
+      console.log(allSelectedConc)
+      console.log(allSelected)
     }
     //adding to table for Points
+    allSelectedPnts = [] //added
     allSelectedRebar = []
+    allSelectedConc = [] //added
     for ( let i = 0; i < allSelected.length; i ++ ) {
           // filtering for points selected
       if (allSelected[ i ].constructor.name == "Points" && allSelected[i].isReference != true && allSelected[i].isRebar != true ) {
-        console.log('you are in the points area')
         allSelectedPnts.push(allSelected[i])
         //selected point is 0xFF7F00
         allSelected[ i ].material.color.set( 0xFF7F00);
-        //console.log(allSelectedPnts)
-        //adding to table
+
         let table = document.getElementById("pointData")
         let row = document.createElement('tr');
         let Xpnt = allSelected[ i ].geometry.attributes.position.array[0] 
@@ -275,12 +330,12 @@ if (event.ctrlKey) {
         Yinput.type = "Number";
         Xinput.value = Xpnt
         Yinput.value = Ypnt
-        Xdata = Xinput
-        Ydata = Yinput
-        Xdata.style ="width: 50%"
-        Ydata.style ="width: 50%"
+        Xdata.appendChild(Xinput) 
+        Ydata.appendChild(Yinput)
         row.appendChild(Xdata)
         row.appendChild(Ydata)
+        Xinput.classList.add("numDropDown") 
+        Yinput.classList.add("numDropDown")
         table.appendChild(row)  
       }
       // filtering for rebar selected
@@ -299,26 +354,59 @@ if (event.ctrlKey) {
         let barData = document.createElement('td')
         var Xinput = document.createElement("input");
         var Yinput = document.createElement("input");
-        var barDiainput = document.createElement("input");
+        var barDiaInput = document.createElement("select");
+
+        var array = [3,4,5,6,7,8,9,10,11,14,18]
+
+        for (var j = 0; j < array.length; j++) {
+          var option = document.createElement("option");
+          option.setAttribute("value", array[j]);
+          option.text = array[j];
+          barDiaInput.appendChild(option);
+        }
+
         Xinput.type = "Number";
         Yinput.type = "Number";
-        barDiainput.type = "Number";
+        //barDiaInput.type = "Number";
         Xinput.value = Xpnt
         Yinput.value = Ypnt
-        barDiainput.value = barDiaSelected
-        Xdata = Xinput
-        Ydata = Yinput
-        barData = barDiainput
-        Xdata.style ="width: 50%"
-        Ydata.style ="width: 50%"
-        barData.style = "width: 50%"
+        barDiaInput.value = barDiaSelected
+        Xinput.classList.add("numDropDown") 
+        Yinput.classList.add("numDropDown")
+        barDiaInput.classList.add("numDropDown")
+        Xdata.appendChild(Xinput) 
+        Ydata.appendChild(Yinput)
+        barData.appendChild(barDiaInput)
         row.appendChild(Xdata)
         row.appendChild(Ydata)
         row.appendChild(barData)
         table.appendChild(row)  
       }
+      else if (allSelected[ i ].constructor.name == "Mesh") {
+          allSelectedConc.push(allSelected[i])
+          allSelected[ i ].material.color.set( 0xFF7F00);
+          //adding to table
+          let table = document.getElementById("concData")
+          let row = document.createElement('tr');
+          let concPnt = allSelectedConc.length
+          console.log(concPnt)
+          
+          let concData = document.createElement('td')
+          concData.innerHTML = concPnt
+          row.appendChild(concData)
+          console.log(concData)
+          console.log(row)
+          table.appendChild(row)  
+      }
     }
   }
+  var pnts = document.getElementById("pointsSelected")
+  console.log(allSelectedPnts)
+  pnts.innerHTML = (allSelectedPnts.length) 
+  var rebars = document.getElementById("rebarSelected")
+  rebars.innerHTML = allSelectedRebar.length
+  var concs = document.getElementById("concSelected")
+  concs.innerHTML = allSelectedConc.length
 } );    
 //making Concrete Shape
 function addConcGeo() {
@@ -340,6 +428,7 @@ function addConcGeo() {
       concShape.lineTo(allSelectedPnts[0].geometry.attributes.position.array[0], allSelectedPnts[0].geometry.attributes.position.array[1])   
     }
   }
+  
 
   const geometry = new THREE.ShapeGeometry( concShape );
   const material = new THREE.MeshStandardMaterial( {color: 0xE5E5E5, transparent: true,
@@ -358,6 +447,9 @@ document.addEventListener('keyup', function (e) {
     }
     for (var pnt in allSelectedRebar) {
       scene.remove(allSelectedRebar[pnt]);
+    }
+    for (var pnt in allSelectedConc) {
+      scene.remove(allSelectedConc[pnt]);
     }
   }
 })
@@ -404,20 +496,18 @@ function addRebar() {
   tempDot.isRebar = true
   tempDot.rebarSize = barDia
   scene.add( tempDot );
-  console.log(tempDot)
 }
 
 
 document.addEventListener('keydown', function (e) {
     //replicate function, use shift key and r to trigger
     if (e.shiftKey == true && (e.key == "R" || e.key == "r")) {
-      
+      //FW, make popup
       var Xreplicate =  parseFloat(prompt("What value of X"))
       var Yreplicate =  parseFloat(prompt("What value of Y"))
       alert( "X value = " + Xreplicate + "Y value = " + Yreplicate)
       
-      //unsure why this is to be divided by 2, it works but is weird
-      for ( let i = 0; i < allSelectedPnts.length/2; i ++ ) {
+      for ( let i = 0; i < allSelectedPnts.length; i ++ ) {
         var xcurrent = allSelectedPnts[i].geometry.attributes.position.array[0]
         var ycurrent = allSelectedPnts[i].geometry.attributes.position.array[1]
         var newX = xcurrent + Xreplicate
@@ -483,11 +573,14 @@ document.getElementById('rebarData').onchange = function (e) {
       target = target.parentNode;
   }
   if (target) {
+      
       var cells = target.getElementsByTagName("input");
+      var rebar = target.getElementsByTagName("select")
+      console.log("right here" + rebar)
       var ind = target.rowIndex - 1
       var x_pnt = cells[0].value
       var y_pnt = cells[1].value
-      var rebar_data = cells[2].value
+      var rebar_data = rebar[0].value
       for (var i = 0; i < cells.length; i++) {
           data.push(cells[i].value);
       }
@@ -511,30 +604,65 @@ function pointSelection() {
   var pointTable =  document.getElementById("pointInfo")
   var rebarTable = document.getElementById("rebarInfo")
   var concTable = document.getElementById("concInfo")
+  var pointBtn = document.getElementById("Points")
+  var rebarBtn = document.getElementById("Rebar")
+  var concBtn = document.getElementById("Conc")
   
   pointTable.style.display = 'inline';
+  pointBtn.style.backgroundColor = '#1a202c';
+  pointBtn.style.color = 'white';
+  
   rebarTable.style.display="none"
+  rebarBtn.style.backgroundColor = 'white';
+  rebarBtn.style.color = '#4a5568'
+  
   concTable.style.display="none"
+  concBtn.style.backgroundColor = 'white';
+  concBtn.style.color = '#4a5568'
 }
 
 function rebarSelection() {
   var pointTable =  document.getElementById("pointInfo")
   var rebarTable = document.getElementById("rebarInfo")
   var concTable = document.getElementById("concInfo")
+  var pointBtn = document.getElementById("Points")
+  var rebarBtn = document.getElementById("Rebar")
+  var concBtn = document.getElementById("Conc")
   
   pointTable.style.display="none"
-  rebarTable.style.display = 'inline	';
+  pointBtn.style.backgroundColor = 'white';
+  pointBtn.style.color = '#4a5568';
+  
+  rebarTable.style.display = 'inline';
+  rebarTable.style.width = "100%"
+  rebarBtn.style.backgroundColor = '#1a202c';
+  rebarBtn.style.color = 'white'
+  
   concTable.style.display="none"
+  concBtn.style.backgroundColor = 'white';
+  concBtn.style.color = '#4a5568'
 }
 
 function concSelection() {
   var pointTable =  document.getElementById("pointInfo")
   var rebarTable = document.getElementById("rebarInfo")
   var concTable = document.getElementById("concInfo")
+  var pointBtn = document.getElementById("Points")
+  var rebarBtn = document.getElementById("Rebar")
+  var concBtn = document.getElementById("Conc")
   
   pointTable.style.display="none"
+  pointBtn.style.backgroundColor = 'white';
+  pointBtn.style.color = '#4a5568';
+  
   rebarTable.style.display="none"
+  rebarBtn.style.backgroundColor = 'white';
+  rebarBtn.style.color = '#4a5568'
+  
   concTable.style.display = 'inline	';
+  concTable.style.width = "100%"
+  concBtn.style.backgroundColor = '#1a202c';
+  concBtn.style.color = 'white'
 }
 
 document.getElementById("Points").onclick = function(){
@@ -549,7 +677,9 @@ document.getElementById("Conc").onclick = function(){
     concSelection();
   }
 
-  //END GUI ITEMS
+//END GUI ITEMS
+
+
 
 let frame = 0
 function animate() {
@@ -573,3 +703,19 @@ function animate() {
   console.log()
 }
 animate();
+
+const mouse = {
+  x: undefined,
+  y: undefined
+}
+
+//this sets the lower right values, not correct right now
+addEventListener('mousemove', (event) => {
+  
+  mouse.x = (((event.clientX - (window.innerWidth*1/6)) / concGui.offsetWidth)*2-1)*16.6667 //FW, need to mouse normalize here with zoom
+  mouse.y = (- ( event.clientY / concGui.offsetHeight )*2+1)*10 //FW, need to mouse normalize here with zoom
+  var X = document.getElementById("xVal")
+  var Y = document.getElementById("yVal")
+  X.innerHTML = String(mouse.x).slice(0,4)
+  Y.innerHTML = String(mouse.y).slice(0,4)
+})
